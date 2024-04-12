e7beebebcd25bfb9272c0b3535d1a8bdc61d1388
zengkaimin <zengkaimin1991@gmail.com>
2024-04-12 17:09:39 +0800

    add get

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ class Item(BaseModel):
 
 app = FastAPI()
 
-# @app.get("/")
-# def read_root():
-#     return {"test"}
+@app.get("/")
+def read_root():
+    return {"test"}
 #test
 
 @app.get("/items/{item_id}")
