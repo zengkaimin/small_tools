335bc80b6fa3b8555967e0841871e6a5cd20069c
zengkaimin <zengkaimin1991@gmail.com>
2024-04-12 17:14:52 +0800

    add summary

--- main.py
+++ main.py
@@ -6,17 +6,28 @@ from pydantic import BaseModel
 
 
 class Item(BaseModel):
+    """_summary_
+
+    Args:
+        BaseModel (_type_): _description_
+    """
     name: str
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
     return {"test"}
 #test
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
+    '''_summary_'''
     print("hahha")
     #test
     print (item_id)
@@ -24,5 +35,7 @@ def read_item(item_id: int, q: Optional[str] = None):
 
 @app.post("/inoreader/starred")
 def rsync_inoreader_starred(item:Item):
+
+
     print('zengkaimin')
     return {"test"}
